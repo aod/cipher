d4d54df127f79fd0840ae4486c09453922ea7aef
Oktay <oktay@yatko.dev>
2022-10-20 11:28:47 +0200

    Add some responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="h-16 bg-black px-2 flex items-center border-b border-white/60">
-      <h1 className="font-serif text-4xl text-white font-bold tracking-wide">
+    <header className="h-12 lg:h-16 bg-black px-2 flex items-center border-b border-white/60">
+      <h1 className="font-serif text-2xl lg:text-4xl text-white font-bold tracking-wide">
         Cipher
       </h1>
     </header>
