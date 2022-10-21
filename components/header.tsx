d6ee98afb04a4227eec56e75831dce51b53f9e6d
Oktay <oktay@yatko.dev>
2022-10-21 19:16:02 +0200

    Add more spacing

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   const link = useLinkData();
 
   return (
-    <header className="h-12 lg:h-16 bg-black px-2 flex justify-between items-center border-b border-white/60">
+    <header className="h-16 lg:h-24 bg-black px-4 lg:px-8 flex justify-between items-center border-b border-white/60">
       <h1 className="font-serif text-2xl lg:text-4xl text-white font-bold tracking-wide">
         Cipher
       </h1>
